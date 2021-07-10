e6cb3d96d5279afdbb91d434e0e6db80b2f5a347
ArtsiomYermakou <artem_ermakov_1999@mail.ru>
2021-07-10 17:13:33 +0300

    UI alert controller

--- learnSwift/ViewController.swift
+++ learnSwift/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var buttonSend: UIButton!
+    @IBOutlet var labelTarget: UILabel!
+    @IBOutlet var inputTarget: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        labelTarget.textColor = .white
+        
+        inputTarget.placeholder = "Enter messages..."
+        inputTarget.clearButtonMode = .whileEditing
+        
+        
+        
     }
-
-
+    
+    @IBAction func sendTextInLabel(_ sender: UIButton) {
+        guard inputTarget.text != "" else { return }
+        
+        if let _ = Double(inputTarget.text!){
+            
+            let alert = UIAlertController(title: "Wrong text \(inputTarget.text!)", message: "Enter your name", preferredStyle: .alert)
+            let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
+            let cancel = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+            
+            present(alert, animated: true, completion: nil)
+            
+            alert.addAction(ok)
+            alert.addAction(cancel)
+            inputTarget.text = ""
+        } else {
+            labelTarget.text = inputTarget.text
+            inputTarget.text = ""
+        }
+    }
+    
 }
 
